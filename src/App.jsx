67c09e372e6ff2c29c696ec34e80931e69a42333
iratololo <iratololo1991@gmail.com>
2023-12-27 19:50:41 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Container } from "./components/Container/Container";
 import { ContactForm } from "./components/ContactForm/ContactForm"
 import { Filter } from "./components/Filter/Filter"
-import {ContactList} from "./components/ContactList/ContactList"
+import { ContactList } from "./components/ContactList/ContactList"
+
+import data from './data.json'
 
 export class App extends Component {
   state = { 
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: data,
     filter: '',
+  }
+
+  componentDidMount() {
+    const localContacts = localStorage.getItem("contacts");
+    if (localContacts&&JSON.parse(localContacts).length>0) {
+      this.setState({
+        contacts: JSON.parse(localContacts),
+      })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
   }
 
   createContact = (data) => {
